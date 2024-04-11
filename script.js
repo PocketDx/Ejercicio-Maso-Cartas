dc2ef8ff4b8a6edb66a84441270135d31274f4df
PocketDx <dairojavierrg@ufps.edu.co>
2024-04-10 21:07:40 -0500

    Segundo Intento
    Debido a falla con las imágenes se prueba de otra manera

--- script.js
+++ script.js
@@ -6,8 +6,11 @@ document.addEventListener("DOMContentLoaded", function() {
     arrayList.forEach(cart => {
         const cardDiv = document.createElement("div");
         cardDiv.classList.add("card");
+        const cardImg = document.createElement("img");
+        cardImg.src = cart.imagenURL;
+        cardImg.alt = cart.carta;
         const cardButton = document.createElement("button");
-        cardButton.textContent = `${cart.carta} (${cart.cantidad})`;
+        cardButton.appendChild(cardImg);
         cardButton.addEventListener("click", function() {
             cart.cantidad++;
             guardarArrayListEnLocalStorage(arrayList);
@@ -26,9 +29,9 @@ function guardarArrayListEnLocalStorage(arrayList) {
 // Define una función para cargar el arraylist desde localStorage
 function cargarArrayListDesdeLocalStorage() {
     const arrayList = JSON.parse(localStorage.getItem('miArrayList')) || [
-        { "numero": 1, "carta": "As", "cantidad": 0 },
-        { "numero": 2, "carta": "Dos", "cantidad": 0 },
-        { "numero": 3, "carta": "Tres", "cantidad": 0 }
+        { "numero": 1, "carta": "As", "cantidad": 0, "imagenURL": "images/1.png" },
+        { "numero": 2, "carta": "Dos", "cantidad": 0, "imagenURL": "https://github.com/PocketDx/Ejercicio-Maso-Cartas/blob/6d80175a7dc81f3a9bf22e3d93e3d2d9ae14722c/images/10.png" },
+        { "numero": 3, "carta": "Tres", "cantidad": 0, "imagenURL": "https://github.com/PocketDx/Ejercicio-Maso-Cartas/blob/main/images/11.png" }
     ];
     return arrayList;
 }
