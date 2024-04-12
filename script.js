2b8b4fe16d3addf8574f2efd10c40be090279d2c
PocketDx <dairojavierrg@ufps.edu.co>
2024-04-11 22:58:38 -0500

    Tercer Intento
    Luego de varios intentos fallidos, reiniciare el proyecto iniciando por el LOGIN usando BOOTSTRAP

--- script.js
+++ script.js
@@ -1,37 +1,23 @@
-document.addEventListener("DOMContentLoaded", function() {
-    const cartasDiv = document.getElementById("cartas");
-    const arrayList = cargarArrayListDesdeLocalStorage();
+// Login
 
-    // Cargar cartas
-    arrayList.forEach(cart => {
-        const cardDiv = document.createElement("div");
-        cardDiv.classList.add("card");
-        const cardImg = document.createElement("img");
-        cardImg.src = cart.imagenURL;
-        cardImg.alt = cart.carta;
-        const cardButton = document.createElement("button");
-        cardButton.appendChild(cardImg);
-        cardButton.addEventListener("click", function() {
-            cart.cantidad++;
-            guardarArrayListEnLocalStorage(arrayList);
-            cardButton.textContent = `${cart.carta} (${cart.cantidad})`;
-        });
-        cardDiv.appendChild(cardButton);
-        cartasDiv.appendChild(cardDiv);
-    });
+document.getElementById('loginForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+    
+    // Obtener los valores del formulario
+    const username = document.getElementById('username').value;
+    const password = document.getElementById('password').value;
+
+    // Verificar las credenciales
+    if (username === 'admin' && password === '1234') {
+        // Almacenar el nombre de usuario en localStorage
+        localStorage.setItem('username', username);
+        
+        // Redireccionar a la página index.html
+        window.location.href = 'index.html';
+    } else {
+        // Mostrar mensaje de error
+        document.getElementById('errorMessage').textContent = 'Credenciales incorrectas';
+    }
 });
 
-// Define una función para guardar el arraylist en localStorage
-function guardarArrayListEnLocalStorage(arrayList) {
-    localStorage.setItem('miArrayList', JSON.stringify(arrayList));
-}
-
-// Define una función para cargar el arraylist desde localStorage
-function cargarArrayListDesdeLocalStorage() {
-    const arrayList = JSON.parse(localStorage.getItem('miArrayList')) || [
-        { "numero": 1, "carta": "As", "cantidad": 0, "imagenURL": "images/1.png" },
-        { "numero": 2, "carta": "Dos", "cantidad": 0, "imagenURL": "https://github.com/PocketDx/Ejercicio-Maso-Cartas/blob/6d80175a7dc81f3a9bf22e3d93e3d2d9ae14722c/images/10.png" },
-        { "numero": 3, "carta": "Tres", "cantidad": 0, "imagenURL": "https://github.com/PocketDx/Ejercicio-Maso-Cartas/blob/main/images/11.png" }
-    ];
-    return arrayList;
-}
+// Index
